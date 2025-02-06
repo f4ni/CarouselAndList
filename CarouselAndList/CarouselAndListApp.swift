554658a9de9d2e0b0e5ddf87d803f2959aa7fbfb
furkan icoz <f_icoz@hotmail.com>
2025-02-06 21:16:38 +0300

    Initial Commit

---
+++ CarouselAndList/CarouselAndListApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselAndListApp.swift
+//  CarouselAndList
+//
+//  Created by Furkan ic on 6.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct CarouselAndListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarouselAndListTests/CarouselAndListTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselAndListTests.swift
+//  CarouselAndListTests
+//
+//  Created by Furkan ic on 6.02.2025.
+//
+
+import Testing
+@testable import CarouselAndList
+
+struct CarouselAndListTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
